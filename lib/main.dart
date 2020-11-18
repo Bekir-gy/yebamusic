4074b6a8237cd60542e55ae36b49b502ac6a8582
Bekir_gy <bekiryegin10@gmail.com>
2020-11-19 01:29:17 +0300

    METOTLAR

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:js_util';
 //deneme
 import 'package:flutter/material.dart';
+import 'package:yebamusic/main.dart';
 
 void main() => runApp(new MyApp());
 
@@ -10,6 +11,30 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  List<Song> _songs;
+  MusicFinder audioPlayer;
+
+  @override
+  void initState() {
+    super.initState();
+    initPlayer();
+  }
+
+  void initPlayer() async {
+    audioPlayer = new MusicFinder();
+    var songs = await MusicFinder.allSongs();
+    songs = new List.from(songs);
+
+    setState(() {
+      _songs = songs;
+    });
+  }
+
+  Future _playLocal(String url) async {
+    final result = await audioPlayer.play(url, isLocal: true);
+    //if (result == 1) setState(() => playerState = PlayerState.playing);
+  }
+
   @override
   Widget build(BuildContext context) {
     Widget home() {
@@ -18,13 +43,14 @@ class _MyAppState extends State<MyApp> {
           title: new Text("YEBA Music Player"),
         ),
         body: new ListView.builder(
-          itemCount: 0,
+          itemCount: _songs.length,
           itemBuilder: (context, int index) {
             return new ListTile(
               leading: new CircleAvatar(
-                child: new Text("ppppp"),
+                child: new Text(_songs[index].title[0]),
               ),
-              title: new Text("data"),
+              title: new Text(_songs[index].title),
+              onTap: () => _playLocal(_songs[index].uri),
             );
           },
         ),
